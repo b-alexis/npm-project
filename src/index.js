2efe4942f9319f735dc008c90687ee6a386387b9
becca <r.alexis.tolentino@gmail.com>
2017-09-13 11:35:06 -0500

    run jquery package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const $ = require ("jquery");
+
 const hello = () => console.log("hello world from webpack!");
 
 hello();
+
+$("body")
+    .html("<h1> Hello from jquery</h1>")
+    .css("background-color", "lightPink");
